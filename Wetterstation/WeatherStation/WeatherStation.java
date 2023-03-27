5b883e2ed4a0272648f986b63ac99a465c6176a7
AbKamouss <128595567+AbKamouss@users.noreply.github.com>
2023-03-27 16:10:44 +0200

    Richtig Wettersatation

--- Wetterstation/WeatherStation/WeatherStation.java
+++ Wetterstation/WeatherStation/WeatherStation.java
@@ -2,27 +2,34 @@ package WeatherStation;
 
 import java.io.FileNotFoundException;
 
+import Interfaces.IPressureSensor;
 import Interfaces.ITempatureReader;
 import Interfaces.IWindspeedDriver;
+import Interfaces.IRainSensor;
+import Interfaces.IUSBTemperaturDriver;
+import Sensors.SerialPressureSensor;
 import Sensors.SerialTemperatureDriver;
 import Sensors.SerialWindspeedDriver;
+import Sensors.USBTemperatureDriver;
+import Sensors.SerialRainSensor;
+import Sensors.SerialPressureSensor;
 
-public class WeatherStation implements ITempatureReader, IWindspeedDriver, IUSBTemperaturDriver, IPressurSensor, IRainSensor{
+public class WeatherStation implements ITempatureReader, IWindspeedDriver, IUSBTemperaturDriver, IPressureSensor, IRainSensor{
 	
 	private SerialWindspeedDriver windspeed;
 	private SerialTemperatureDriver tempature;
 	private SerialRainSensor rain;
-	private SerialPressureDriver pressure;
-	private USBTemperaturDriver usbTemperatur;
+	private SerialPressureSensor pressure;
+	private USBTemperatureDriver usbTemperatur;
 	
-	public WeatherStation(SerialWindspeedDriver windspeed,  SerialTemperatureDriver tempature, SerialRainSensor rain, SerialPressureDriver pressure) {
-		this.windspeed = windspeed;
+	public WeatherStation(SerialWindspeedDriver windspeed,  SerialTemperatureDriver tempature, SerialRainSensor rain, SerialPressureSensor pressure) {
+		this.windspeed = windspeed; 
 		this.tempature = tempature;
 		this.pressure = pressure;
 		this.rain = rain;
 	}
 	
-	public WeatherStation(SerialWindspeedDriver windspeed,  USBTemperaturDriver usbTemperatur, SerialRainSensor rain, SerialPressureDriver pressure) {
+	public WeatherStation(SerialWindspeedDriver windspeed,  USBTemperatureDriver usbTemperatur, SerialRainSensor rain, SerialPressureSensor pressure) {
 		this.windspeed = windspeed;
 		this.usbTemperatur = usbTemperatur;
 		this.pressure = pressure;
@@ -40,20 +47,16 @@ public class WeatherStation implements ITempatureReader, IWindspeedDriver, IUSBT
 	public double getCurrentWindspeed() {
 		return windspeed.currentWindspeed();
 	}
-	
-	@Ovveride
-	public double getCurrentPressure {
-		return pressure.getSensorValue();
+
+	@Override
+	public double getSensorValue() {
+		return pressure.getSensorValue() + rain.getSensorValue();
 	}
-	
-	@Ovveride
-	public double getCurrentRain() {
-		return rain.getSensorValue();
-	}
-	
-	@Ovveride
+
+	@Override
 	public int getCurrentTemperature() {
-		return usbTemperatur.getTemperaturValue();
+	return usbTemperatur.getTemperatureValue();
 	}
+	
 
 }
