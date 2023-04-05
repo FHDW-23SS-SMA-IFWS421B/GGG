b8e125b2c19c82738807c85ace5d22904ec73ab2
Gabriel Geishüttner <info@geishuettner.com>
2023-04-05 15:55:26 +0200

    BotSystem

---
+++ Chatsystem/userInterface/UserInterface.java
@@ -0,0 +1,14 @@
+package userInterface;
+
+import java.io.FileNotFoundException;
+
+public class UserInterface {
+
+	public static void UserInterface(String[] args) throws FileNotFoundException {
+		
+		System.out.println("Text");
+	
+	}
+
+
+}
